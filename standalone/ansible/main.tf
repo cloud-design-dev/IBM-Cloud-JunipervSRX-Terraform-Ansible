a5b530feb1bc9bdfa0cc320c36556adce825ec12
Ryan Tiffany <greyhoundforty@users.noreply.github.com>
2021-04-21 08:47:38 -0500

    Create main.tf

---
+++ standalone/ansible/main.tf
@@ -0,0 +1,13 @@
+resource "local_file" "ansible_inventory_vars" {
+  content = templatefile("${path.module}/templates/deployment_vars.tmpl",
+    {
+      public_vlan = var.public_vlan
+      private_vlan      = var.private_vlan
+      private_subnet = var.private_subnet
+      public_subnet = var.public_subnet
+      private_subnet_gateway = "${cidrhost(var.private_subnet, 1)}/26"
+      public_subnet_gateway = "${cidrhost(var.public_subnet, 1)}/28"
+    }
+  )
+  filename = "${path.module}/playbooks/deployment_vars.yml"
+}
